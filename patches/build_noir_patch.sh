bd13a185fee9e846f606d870bf40a64eb01fe068
takabrk <takabrk@kvf.biglobe.ne.jp>
2021-12-23 14:07:46 +0900

    update20211223

--- patches/build_noir_patch.sh
+++ patches/build_noir_patch.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #noir linux kernel patchsets build script
 #Created by takamitsu hamada
-#December 19,2021
+#December 23,2021
 
-VERSIONPOINT="5.15.10"
+VERSIONPOINT="5.15.11"
 NOIR_VERSION="noir"
 truncate noir.patch --size 0
 truncate noir_base/custom_config.patch --size 0
