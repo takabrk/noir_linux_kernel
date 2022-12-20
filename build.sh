16669a8fad7cd2c3f1da974852f48d3ec0820368
takabrk <takabrk@kvf.biglobe.ne.jp>
2022-12-20 15:37:55 +0900

    update20221220

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #custom linux kernel build script
 #Created by takamitsu hamada
-#December 13,2022
+#December 20,2022
 
 . ./config
 
@@ -15,7 +15,7 @@ done
 
 case $e_num in
 #build noir.patch
-    600)
+    patch)
         truncate noir.patch --size 0
         truncate patches/noir_base/custom_config.patch --size 0
 
@@ -27,69 +27,39 @@ case $e_num in
             patches/other6/zen_interactive_tune.patch \
             patches/other6/zen_other.patch \
             patches/other6/patch-6.1-rc7-rt5.patch \
-            patches/other6/LL.patch \
             patches/other6/0001-futex-patches.patch \
             patches/other6/0001-clearlinux-6.1-introduce-clearlinux-patchset.patch \
             patches/other6/0001-winesync-Introduce-the-winesync-driver-and-character.patch \
             patches/other6/0001-tcp_bbr2-introduce-BBRv2.patch \
             patches/other6/0001-zswap-patches.patch \
             patches/other6/0001-arch-patches.patch \
+            patches/other6/0001-amd_pstate-Add-guided-autonomous-mode-support.patch \
+            patches/other6/0008-XANMOD-kconfig-add-500Hz-timer-interrupt-kernel-conf.patch \
+            patches/other6/0009-XANMOD-dcache-cache_pressure-50-decreases-the-rate-a.patch \
+            patches/other6/0010-XANMOD-mm-vmscan-vm_swappiness-30-decreases-the-amou.patch \
+            patches/other6/set_8megabytes_for_address_space-level_file.patch \
             > noir.patch
             ;;
 
-    base)
+    source)
            wget https://mirrors.edge.kernel.org/pub/linux/kernel/v6.x/linux-$VERSIONBASE.tar.xz
            tar -Jxvf linux-$VERSIONBASE.tar.xz
-           #wget https://git.kernel.org/torvalds/t/linux-$VERSIONBASE.tar.gz
-           #tar -zxvf linux-$VERSIONBASE.tar.gz
            cd linux-$VERSIONBASE
            patch -p1 < ../noir.patch
            cd ../
-           mv linux-$VERSIONBASE linux-$VERSIONPOINT-noir
-           #rm -r linux-$VERSIONBASE.tar.gz
+           mv linux-$VERSIONBASE linux-$VERSIONPOINT-$NOIR_VERSION
            rm -r linux-$VERSIONBASE.tar.xz
            ;;
-    core)
-           cd linux-$VERSIONPOINT-noir
+    build)
+           cd linux-$VERSIONPOINT-$NOIR_VERSION
            make menuconfig
            #make xconfig
            sudo make-kpkg clean
            time sudo make-kpkg -j3 --initrd linux_image linux_headers
-           #cd linux-$VERSIONBASE-noir
-           #sudo make modules_install -j4
-           #cd ../
-           #rm -r linux_modules
-           #mkdir linux_modules
-           #cd linux-$VERSIONPOINT-noir
-           #make INSTALL_MOD_PATH=../linux_modules modules_install -j4
            sudo make-kpkg clean
            cd ../
-           #zip -r linux-$VERSIONPOINT-noir.zip linux-$VERSIONPOINT-noir
-           sudo rm -r linux-$VERSIONPOINT-noir
+           sudo rm -r linux-$VERSIONPOINT-$NOIR_VERSION
            sudo dpkg -i *.deb
-           sudo update-grub
-           ;;
-
-    distcc)
-           cd linux-$VERSIONPOINT-noir
-           make xconfig
-           sudo make-kpkg clean
-           #sudo CC="distcc gcc" CXX="distcc g++" make-kpkg -j4 --initrd linux_image linux_headers
-           #sudo CONCURRENCY_LEVEL=4 MAKEFLAGS="CC=distcc gcc" make-kpkg -j4 --initrd linux_image linux_headers
-            sudo MAKEFLAGS="CC=distcc" BUILD_TIME="/usr/bin/time" CONCURRENCY_LEVEL=$(distcc -j) make-kpkg --rootcmd fakeroot --initrd kernel_image kernel_headers
-           cd ../
-           sudo dpkg -i *.deb
-           cd linux-$VERSIONPOINT-noir
-           #sudo make modules_install -j4
-           #cd ../
-           #rm -r linux_modules
-           #mkdir linux_modules
-           #cd linux-$VERSIONBASE-noir
-           #make INSTALL_MOD_PATH=../linux_modules modules_install -j4
-           sudo make-kpkg clean
-           cd ../
-           #zip -r linux-$VERSIONPOINT-noir.zip linux-$VERSIONPOINT-noir
-           sudo rm -r linux-$VERSIONPOINT-noir
            sudo update-grub
            ;;
 esac
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 Custom linux kernel "Noir Linux kernel"
 Web site URL : http://vsrx.work
 Created by takamitsu hamada
-December 13,2022
+December 20,2022
 
 このカスタムカーネルは、Ubuntu/Debian向けです。
 Ubuntu公式のカーネルと比較して、レスポンス性能やデスクトップ用途・ゲーミング用途におけるパフォーマンスを大きく引き上げる事が出来ます。
@@ -19,30 +19,30 @@ $ ./performanceup.sh
 noir.patchというファイルは、それらのパッチを統合した物であり、これをバニラカーネル( https://www.kernel.org )のソースコードに当てる事で、カスタムカーネル用のソースコードを作る事も可能です。
 
 ◇Noir Linux Kernelパッチの組み立て
-$ ./build.sh -e 600
+$ ./build.sh -e patch
 
 ◇バニラカーネルのソースコードのダウンロードとパッチ当て（カスタムカーネルのビルド作業は、ここから始める）
 - ソースコード取得・パッチ当て
-$ ./build.sh -e base
+$ ./build.sh -e source
 
 ◇前述を行った後にカスタムカーネルのビルドとインストール
-$ ./build.sh -e core
+$ ./build.sh -e build
 
 
-- Built on the GCC 11.2.0
+- Built on the GCC 12.1.0
 - CPU scheduler -> CFS
 - Default I/O scheduler -> Kyber
 - Processor family -> Generic X86_64
 - Kernel Compression mode -> zstd
 - Preemption Model -> Full Preemptible Kernel(low latency desktop)
-- CPU Timer frequency -> 750Hz
+- CPU Timer frequency -> 500Hz
 - RCU boost delay -> 339
 - Timer tick handling -> Full dynticks system
 - Default CPUFreq Governor -> schedutil
 - CPU idle governor -> TEO
-- nr_migrate = 256
-- rq_affinity = 2
 - vm_swappiness = 30
+- VM_READAHEAD_PAGES=8MB
+- dcache-cache_pressure=50
 - futex support
 - PCIe ACS support
 - OpenRGB support
@@ -52,13 +52,14 @@ $ ./build.sh -e core
 - WineSync support
 - BBR2 support
 - zswap support
+- AMD p-state support
 
 [patches]
 - linux update patch( https://www.kernel.org/ )
-- Zen( https://github.com/zen-kernel/zen-kernel/tree/5.15/master )
-- PREEMPT RT( https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/ )
+- Zen patch( https://github.com/zen-kernel/zen-kernel/tree/6.1/master )
+- PREEMPT RT patch( https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/ )
 - https://github.com/sirlucjan/kernel-patches 
-- https://github.com/Frogging-Family/linux-tkg/tree/master/linux-tkg-patches
+- https://github.com/xanmod/linux-patches/tree/master/linux-6.1.y-xanmod/xanmod
 
 ◇I/Oスケジューラー確認方法
 現在使っているI/Oスケジューラーの確認方法は、端末で以下のコマンドを実行する事で出来ます。
