57f5e312c53dad39521d12a157915b54fa0e9720
takabrk <takabrk@kvf.biglobe.ne.jp>
2024-03-13 13:46:37 +0900

    update20240313

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #custom linux kernel build script
 #Created by takamitsu hamada
-#March 7,2024
+#March 13,2024
 
 . ./config
 
@@ -27,18 +27,21 @@ case $e_num in
 
 #build custom_config.patch
         diff -Naur /dev/null patches/noir_base/.config | sed 1i"diff --git a/.config b/.config\nnew file mode 100644\nindex 000000000000..dcbcaa389249" > patches/noir_base/custom_config.patch
-        cat patches/linux/patch-$VERSIONPOINT \
-            patches/noir_base/noir_base.patch \
+        if [ -e patches/linux/patch-$VERSIONPOINT ]; then
+            cat patches/linux/patch-$VERSIONPOINT > noir.patch
+        fi
+        cat patches/noir_base/noir_base.patch \
             patches/noir_base/custom_config.patch \
-            patches/other/linux-v6.7-zen1.patch \
-            patches/other/patch-6.7-rc5-rt5.patch \
-            patches/other/0001-futex-Add-entry-point-for-FUTEX_WAIT_MULTIPLE-opcode.patch \
-            patches/other/0001-winesync-Introduce-the-winesync-driver-and-character.patch \
+            patches/other/linux-v6.8-zen1.patch \
+            patches/other/patch-6.8-rt7.patch \
+            patches/other/0001-amd-pstate-patches.patch \
+            patches/other/0001-futex-6.8-Add-entry-point-for-FUTEX_WAIT_MULTIPLE-op.patch \
+            patches/other/0007-v6.8-winesync.patch \
             patches/other/0010-XANMOD-kconfig-add-500Hz-timer-interrupt-kernel-conf.patch \
             patches/other/0011-XANMOD-dcache-cache_pressure-50-decreases-the-rate-a.patch \
             patches/other/0012-XANMOD-mm-vmscan-vm_swappiness-30-decreases-the-amou.patch \
-            patches/other/0001-bcachefs-6.7-merge-changes-from-dev-tree.patch \
-            > noir.patch
+            patches/other/0001-bcachefs-6.8-merge-changes-from-dev-tree.patch \
+            >> noir.patch
            ;;
     vanilla)  
             wget https://mirrors.edge.kernel.org/pub/linux/kernel/v6.x/linux-$VERSIONBASE.tar.xz
