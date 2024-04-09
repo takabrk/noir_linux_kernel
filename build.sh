ec82221f352004fad5031e663193bbe8ef80d747
takabrk <takabrk@kvf.biglobe.ne.jp>
2024-04-09 20:20:02 +0900

    update20240409

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #custom linux kernel build script
 #Created by takamitsu hamada
-#March 13,2024
+#March 30,2024
 
 . ./config
 
@@ -22,25 +22,21 @@ fi
 case $e_num in
 #build noir.patch
     patch)
-        truncate noir.patch --size 0
-        truncate patches/noir_base/custom_config.patch --size 0
-
 #build custom_config.patch
-        diff -Naur /dev/null patches/noir_base/.config | sed 1i"diff --git a/.config b/.config\nnew file mode 100644\nindex 000000000000..dcbcaa389249" > patches/noir_base/custom_config.patch
         if [ -e patches/linux/patch-$VERSIONPOINT ]; then
             cat patches/linux/patch-$VERSIONPOINT > noir.patch
         fi
         cat patches/noir_base/noir_base.patch \
-            patches/noir_base/custom_config.patch \
+            patches/noir_base/kconfig-add-500Hz-timer-interrupt-kernel-conf.patch \
+            patches/noir_base/dcache-cache_pressure-50-decreases-the-rate-a.patch \
+            patches/noir_base/mm-vmscan-vm_swappiness-30-decreases-the-amou.patch \
             patches/other/linux-v6.8-zen1.patch \
-            patches/other/patch-6.8-rt7.patch \
+            patches/other/patch-6.8.2-rt11.patch \
             patches/other/0001-amd-pstate-patches.patch \
             patches/other/0001-futex-6.8-Add-entry-point-for-FUTEX_WAIT_MULTIPLE-op.patch \
             patches/other/0007-v6.8-winesync.patch \
-            patches/other/0010-XANMOD-kconfig-add-500Hz-timer-interrupt-kernel-conf.patch \
-            patches/other/0011-XANMOD-dcache-cache_pressure-50-decreases-the-rate-a.patch \
-            patches/other/0012-XANMOD-mm-vmscan-vm_swappiness-30-decreases-the-amou.patch \
             patches/other/0001-bcachefs-6.8-merge-changes-from-dev-tree.patch \
+            patches/noir_base/RCU_BOOST_339.patch\
             >> noir.patch
            ;;
     vanilla)  
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 Custom linux kernel "Noir Linux kernel"
 Web site URL : http://vsrx.work
 Created by takamitsu hamada
-March 13,2024
+April 9,2024
 
 このカスタムカーネルは、Ubuntu向けです。
 リアルタイム性能・レスポンス性能の向上を図ります。
