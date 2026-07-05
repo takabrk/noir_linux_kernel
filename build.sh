dfa7b4f3ee6754679072468a3dcca6d21734f433
takabrk <taka.brk@gmail.com>
2026-07-05 10:43:43 +0900

    update20260705

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #custom linux kernel build script
 #Created by takamitsu_h
-#July 1,2026
+#July 5,2026
 
 . ./config
 
@@ -26,11 +26,9 @@ case $e_num in
         cd ../../
         cd patches/other
         rm -r *.patch
-        wget https://www.kernel.org/pub/linux/kernel/projects/rt/7.1/patch-7.1.1-rt2.patch.xz
-        unxz patch-7.1.1-rt2.patch.xz
-        rm -r patch-7.1.1-rt2.patch.xz
         wget https://raw.githubusercontent.com/sirlucjan/kernel-patches/refs/heads/master/$VERSIONBASE/futex-patches/0001-futex-$VERSIONBASE-Add-entry-point-for-FUTEX_WAIT_MULTIPLE-op.patch
         wget https://raw.githubusercontent.com/Frogging-Family/linux-tkg/refs/heads/master/linux-tkg-patches/$VERSIONBASE/0002-clear-patches.patch
+        wget https://raw.githubusercontent.com/sirlucjan/kernel-patches/refs/heads/master/$VERSIONBASE/bbr3-patches/0001-tcp-bbr3-add-BBRv3-congestion-control.patch
         wget https://raw.githubusercontent.com/Frogging-Family/linux-tkg/refs/heads/master/linux-tkg-patches/$VERSIONBASE/0013-optimize_harder_O3.patch
         wget https://raw.githubusercontent.com/Frogging-Family/linux-tkg/refs/heads/master/linux-tkg-patches/$VERSIONBASE/0006-add-acs-overrides_iommu.patch
         wget https://raw.githubusercontent.com/Frogging-Family/linux-tkg/refs/heads/master/linux-tkg-patches/$VERSIONBASE/0014-OpenRGB.patch
@@ -42,14 +40,15 @@ case $e_num in
             cat patches/linux/patch-$VERSIONPOINT >> noir.patch
         fi
         case $f_num in
-            rt)
+            noir)
                 cat patches/noir_base/noir_base.patch \
-                    patches/other/patch-7.1.1-rt2.patch \
                     >> noir.patch
             ;;
         esac
-        cat patches/other/0001-futex-$VERSIONBASE-Add-entry-point-for-FUTEX_WAIT_MULTIPLE-op.patch \
+        cat patches/sched_Flatten_the_pick.patch \
+            patches/other/0001-futex-$VERSIONBASE-Add-entry-point-for-FUTEX_WAIT_MULTIPLE-op.patch \
             patches/other/0002-clear-patches.patch \
+            patches/other/0001-tcp-bbr3-add-BBRv3-congestion-control.patch \
             patches/noir_base/default_kyber.patch \
             patches/other/0013-optimize_harder_O3.patch \
             patches/other/0006-add-acs-overrides_iommu.patch \
@@ -60,14 +59,14 @@ case $e_num in
            ;;
     vanilla)
             case $f_num in
-               rt)
+               noir)
                    wget https://mirrors.edge.kernel.org/pub/linux/kernel/v$LINUX_MAJOR.x/linux-$VERSIONBASE.tar.xz
                    ;;
             esac 
             ;;
     source)
            case $f_num in
-               rt)
+               noir)
                    tar -Jxvf linux-$VERSIONBASE.tar.xz
                    cd linux-$VERSIONBASE
                    patch -p1 < ../noir.patch
@@ -78,7 +77,7 @@ case $e_num in
            ;;                
     build)
            case $f_num in
-               rt)
+               noir)
                    JOBS=$(grep processor /proc/cpuinfo | wc -l)
                    echo "Threads : $JOBS"
                    cd linux-$VERSIONPOINT-$NOIR_VERSION
@@ -95,7 +94,7 @@ case $e_num in
            ;;
     install_kernel)
            case $f_num in
-               rt)
+               noir)
                    cd linux-$VERSIONPOINT-$NOIR_VERSION
                    sudo make clean
                    cd ../
